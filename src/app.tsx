4f42b31d3d1e35e79cf99659f7aefc3096776a07
Arthur <Arth.valadao@gmail.com>
2024-02-08 00:39:42 -0300

    feat: componentizing the NoteCard

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,5 @@
 import logo from './assets/logo-nlw-expert.svg'
+import { NoteCard } from './components/note-card'
 
 export function App() {
   return (
@@ -16,44 +17,11 @@ export function App() {
       <div className="h-px bg-slate-700" />
 
       <div className="grid grid-cols-3 auto-rows-[250px] gap-6">
-        <div className="rounded-md bg-slate-700 p-5 space-y-3">
-          <span className="text-sm font-medium text-slate-200"
-          >
-            Adicionar nota
-          </span>
-          <p className='text-sm leading-6 text-slate-400'
-          >
-            Grave uma nota em áudio que será convertida para texto automaticamente.
-          </p>
-        </div>
 
-        <div className="rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative">
-          <span className="text-sm font-medium text-slate-300"
-          >
-            há 2 dias
-          </span>
-          <p className='text-sm leading-6 text-slate-400'
-          >
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente aliquid ex adipisci maiores rerum qui voluptatibus, dolor perspiciatis omnis vitae nulla laudantium modi ipsa. Facere eum error enim doloribus soluta?
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente aliquid ex adipisci maiores rerum qui voluptatibus, dolor perspiciatis omnis vitae nulla laudantium modi ipsa. Facere eum error enim doloribus soluta?
-          </p>
-
-          <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60  to-black/0 pointer-events-none" />
-        </div>
-
-        <div className="rounded-md bg-slate-800 p-5 space-y-3 relative">
-          <span className="text-sm font-medium text-slate-300"
-          >
-            há 4 dias
-          </span>
-          <p className='text-sm leading-6 text-slate-400'
-          >
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure est quasi obcaecati quae cum numquam fuga aut, saepe alias pariatur nesciunt adipisci, voluptate consequatur quam dolorem hic a? Mollitia, eos!
-          </p>
-
-          <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60  to-black/0 pointer-events-none" />
-        </div>
-
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
       </div>
     </div>
   )
